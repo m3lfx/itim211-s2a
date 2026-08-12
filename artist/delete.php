47d03c5c418816586e63ac3e68018ed8cf6cd92c
rommel dalisay <rommelmdalisay@gmail.com>
2026-08-12 16:25:20 +0800

    artist crud

---
+++ artist/delete.php
@@ -0,0 +1,9 @@
+<?php
+include('../includes/config.php');
+$artist_id = $_GET['id'];
+$sql = "DELETE FROM artists WHERE artist_id = $artist_id";
+// echo $sql;
+$result = mysqli_query($conn, $sql);
+if ($result) {
+    header("Location: index.php");
+}
--- artist/index.php
+++ artist/index.php
@@ -1,2 +1,31 @@
 <?php
-echo "artist added";
+include('../includes/config.php');
+include('../includes/header.php');
+
+$sql = "SELECT * FROM artists";
+$result = mysqli_query($conn, $sql);
+?>
+
+<body>
+    <a href="create.php" class="btn btn-primary btn-lg " role="button" aria-disabled="true">Add artist</a></p>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <th>Artist id</th>
+            <th>Artist name</th>
+            <th>Artist country</th>
+        </thead>
+
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            // echo "<td><img src={$row['img_path']} width='150' height='150' /> </td>";
+            echo "<td>{$row['artist_id']}</td>";
+            echo "<td>{$row['artist_name']}</td>";
+            echo "<td>{$row['country']}</td>";
+
+            echo "<td><a href='edit.php?id={$row['artist_id']}'><i class='fa-regular fa-pen-to-square' style='color: blue'></i></a><a href='delete.php?id={$row['artist_id']}'><i class='fa-solid fa-trash' style='color: red'></i></a></td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
