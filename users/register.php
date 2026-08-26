e4b471adaabf769e9628bf366484a69e49e5b562
rommel dalisay <rommelmdalisay@gmail.com>
2026-08-26 15:12:54 +0800

    register

---
+++ users/register.php
@@ -0,0 +1,31 @@
+<?php
+// CREATE TABLE users(
+// user_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+// email VARCHAR(100),
+// password TEXT,
+// status enum('active','inactive'),
+// created_at DATE
+// )
+
+
+
+)
+
+include("../includes/header.php");
+include("../includes/config.php");
+?>
+<div class="container-fluid container-lg">
+    <form action="store.php" method="POST">
+        <div class="mb-3">
+            <label for="email" class="form-label">email</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+</div>
