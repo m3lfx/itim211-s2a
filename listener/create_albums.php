cdef3a8ac08152c0ec97d9b7ebeefa45d9727a81
rommel dalisay <rommelmdalisay@gmail.com>
2026-09-02 14:56:59 +0800

    album crud

--- listener/create_albums.php
+++ listener/create_albums.php
@@ -5,22 +5,43 @@ var_dump($_SESSION);
 include("../includes/header.php");
 include("../includes/config.php");
 
+
+
 $listener_sql = "SELECT listener_id FROM listeners WHERE user_id = {$_SESSION['user_id']} LIMIT 1";
 var_dump($listener_sql);
 $result = mysqli_query($conn, $listener_sql);
 $row = mysqli_fetch_assoc($result);
 $listener_id = (int)$row['listener_id'];
-var_dump($listener_id);
+// var_dump($listener_id);
+// if (isset($_POST['albums'])) {
+//     $album_ids = $_POST['albums'];
+//     var_dump($album_ids);
+//     foreach ($album_ids as $album_id) {
+//         // echo $album_id;
+
+//         $sql1 = "INSERT INTO album_listener (listener_id, album_id) VALUES($listener_id, $album_id )";
+//         $result = mysqli_query($conn, $sql1);
+//     }
+//     // unset($_POST);
+//     $_POST['albums'] = array();
+//     header("Location: index.php");
+// }
+
 if (isset($_POST['albums'])) {
     $album_ids = $_POST['albums'];
     var_dump($album_ids);
+    $result = mysqli_query($conn, "DELETE FROM album_listener WHERE  listener_id = $listener_id");
     foreach ($album_ids as $album_id) {
         // echo $album_id;
 
-        $sql1 = "INSERT INTO album_listener (listener_id, album_id) VALUES($listener_id, $album_id )";
+        $sql1 = "INSERT INTO album_listener (listener_id,album_id) VALUES({$_SESSION['listener_id']}, $album_id )";
         $result = mysqli_query($conn, $sql1);
     }
-    // unset($_POST);
+    unset($_POST);
     $_POST['albums'] = array();
     header("Location: index.php");
+} else {
+    $result = mysqli_query($conn, "DELETE FROM album_listener WHERE  listener_id = {$_SESSION['listener_id']}");
+    if ($result)
+        header("Location: index.php");
 }
--- listener/index.php
+++ listener/index.php
@@ -3,23 +3,95 @@ session_start();
 include("../includes/header.php");
 require("../includes/config.php");
 
+$sql = "SELECT l.listener_id FROM users u INNER JOIN listeners l ON (u.user_id = l.user_id) WHERE u.user_id = {$_SESSION['user_id']} LIMIT 1";
+$query = mysqli_query($conn, $sql);
+$listener = mysqli_fetch_assoc($query);
+// $listener_id = $listener['listener_id'];
+$_SESSION['listener_id'] = (int) $listener['listener_id'];
 
-$album_sql = "SELECT * FROM albums";
-$albums = mysqli_query($conn, $album_sql);
+// $album_sql = "SELECT * FROM albums";
+// $albums = mysqli_query($conn, $album_sql);
+
+$sql1 = "SELECT * FROM albums";
+$albums = mysqli_query($conn, $sql1);
+
+$sql2 = "SELECT * FROM albums a INNER JOIN album_listener al ON (a.album_id = al.album_id) INNER JOIN artists ar ON (ar.artist_id = a.artist_id) WHERE al.listener_id  = {$_SESSION['listener_id']}";
+$myAlbums = mysqli_query($conn, $sql2);
 ?>
+<!-- <div class="container-fluid container-lg"> -->
+<!-- <form action="create_albums.php" method="POST"> -->
+<?php
+// if (mysqli_num_rows($albums) > 0) {
+
+//     while ($row = mysqli_fetch_assoc($albums)) {
+
+//         echo "<div class='form-check'>
+//             <input class='form-check-input' type='checkbox' value='{$row['album_id']}' id='flexCheckDefault' name='albums[]'>
+//             <label class='form-check-label' for='flexCheckDefault'>
+//                 {$row['album_name']}
+//             </label>
+//             </div>";
+//     }
+// }
+
+?>
+<!-- <button type="submit" class="btn btn-primary">update list</button>
+    </form>
+</div> -->
+
+<div class="container-fluid container-lg">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Album Name</th>
+                <th>Artist Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+
+            if (mysqli_num_rows($myAlbums) > 0) {
+                while ($row = mysqli_fetch_assoc($myAlbums)) {
+                    // var_dump($row);
+                    $album_ids[] = $row['album_id'];
+                    print "<tr>";
+                    echo "<td> <a href='review.php?album_id={$row['album_id']}' >{$row['album_name']}</a></td>";
+                    echo "<td>" . $row['artist_name'] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                $album_ids = array();
+                echo "<p>You have no albums</p>";
+            }
+            print_r($album_ids);
+
+            ?>
+        </tbody>
+    </table>
+
+</div>
+
 <div class="container-fluid container-lg">
     <form action="create_albums.php" method="POST">
         <?php
         if (mysqli_num_rows($albums) > 0) {
 
             while ($row = mysqli_fetch_assoc($albums)) {
-
-                echo "<div class='form-check'>
+                if (in_array($row['album_id'], $album_ids)) {
+                    echo "<div class='form-check'>
+                    <input class='form-check-input' type='checkbox' value='{$row['album_id']}' id='flexCheckDefault' name='albums[]' checked>
+                    <label class='form-check-label' for='flexCheckDefault'>
+                        {$row['album_name']}
+                    </label>
+                    </div>";
+                } else {
+                    echo "<div class='form-check'>
                     <input class='form-check-input' type='checkbox' value='{$row['album_id']}' id='flexCheckDefault' name='albums[]'>
                     <label class='form-check-label' for='flexCheckDefault'>
                         {$row['album_name']}
                     </label>
                     </div>";
+                }
             }
         }
 
